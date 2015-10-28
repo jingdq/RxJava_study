4ae22bbaca5aa075fdd084fff4829794a6e5f043
jingdongqi <jingdongqi@benmu-health.com>
2015-10-28 11:02:28 +0800

    add operator

--- src/com/jing/rxjava/HelloWorld.java
+++ src/com/jing/rxjava/HelloWorld.java
@@ -3,6 +3,7 @@ package com.jing.rxjava;
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 /**
  * Created by jimi on 10/28/15.
@@ -59,7 +60,29 @@ public class HelloWorld {
 //        observable.subscribe(subscriber);
 
 
-        Observable.just("hello rxjava").subscribe(s->System.out.println(s));
+        Observable.just("hello rxjava").subscribe(s -> System.out.println(s));
+
+
+        //operator
+
+
+        Observable.just("#Basic Markdown to HTML#").map(new Func1<String, Object>() {
+
+
+            @Override
+            public Object call(String s) {
+
+
+                if (s != null && s.startsWith("#")) {
+
+                    return "<h1>" + s.substring(1, s.length()) + "</h1>";
+                }
+
+                return null;
+            }
+        }).subscribe(s -> System.out.println(s))
+        ;
+
 
     }
 
