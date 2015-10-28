cf37b8b68e0010c547a87a1a7011da2156e1ca93
jingdongqi <jingdongqi@benmu-health.com>
2015-10-28 10:51:17 +0800

    精简写法

--- src/com/jing/rxjava/HelloWorld.java
+++ src/com/jing/rxjava/HelloWorld.java
@@ -2,48 +2,64 @@ package com.jing.rxjava;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Action1;
 
 /**
- * Created by jingdongqi on 10/28/15.
+ * Created by jimi on 10/28/15.
  */
 public class HelloWorld {
     public static void main(String[] args) {
 
-        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+//        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+//
+//
+//            @Override
+//            public void call(Subscriber<? super String> subscriber) {
+//                subscriber.onNext("Hello rxjava");
+//                subscriber.onCompleted();
+//
+//
+//            }
+//        });
+//
+//
+//        Subscriber<String> subscriber = new Subscriber<String>() {
+//            @Override
+//            public void onCompleted() {
+//
+//            }
+//
+//            @Override
+//            public void onError(Throwable throwable) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(String s) {
+//
+//                System.out.println(s);
+//
+//            }
+//        };
+//
+//
+//        observable.subscribe(subscriber);
 
 
-            @Override
-            public void call(Subscriber<? super String> subscriber) {
-                subscriber.onNext("Hello rxjava");
-                subscriber.onCompleted();
+//        Observable<String> observable = Observable.just("Hello Rxjava");
+//
+//
+//        Action1<String> subscriber = new Action1<String>() {
+//            @Override
+//            public void call(String s) {
+//                System.out.println(s);
+//            }
+//        };
+//
+//        observable.subscribe(subscriber);
 
 
-            }
-        });
-
-
-        Subscriber<String> subscriber = new Subscriber<String>() {
-            @Override
-            public void onCompleted() {
-
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-
-            }
-
-            @Override
-            public void onNext(String s) {
-
-                System.out.println(s);
-
-            }
-        };
-
-
-        observable.subscribe(subscriber);
-
+        Observable.just("hello rxjava").subscribe(s->System.out.println(s));
 
     }
 
